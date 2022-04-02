eba09b4ec0c0c63543799458c68707c43d6e59da
khuzaemah <khuza.emah24@gmail.com>
2022-04-03 05:44:10 +0700

    menambahkan data user penata rias seeder

--- penata-rias/database/seeders/ProfileSeeder.php
+++ penata-rias/database/seeders/ProfileSeeder.php
@@ -18,7 +18,37 @@ class ProfileSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('admin123'),
-            // 'role' => 'admin'
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Sanggar Rias Hendi & Wedding Decoration',
+            'email' => 'rias_hendi@gmail.com',
+            'password' => bcrypt('password'),
+            'nama_lengkap' => 'Rias Hendi',
+            'alamat' => 'Ds. Kertanegara Blok 1 Kec. Hargelius, Kab. Indramayu 45264',
+            'whatsapp' => '082214271664',
+            'sosial_media' => '-',
+            'profil_bio' => '-',
+            'foto_profil' => '',
+            'tempat_kerja' => 'Hargelius',
+            'status' => 'online',
+            'role' => 'mua',
+        ]);
+
+        User::create([
+            'name' => "Vin's Salon Salon Khusus Wanita dan Rias Pengantin",
+            'email' => 'vins_salon@gmail.com',
+            'password' => bcrypt('password'),
+            'nama_lengkap' => 'Vins Salon',
+            'alamat' => 'Jl. Raya Sumur Watu Pedati I - Jatimulya, Kec.Terisi Kab. Indramayu 45267',
+            'whatsapp' => '082214271664',
+            'sosial_media' => '-',
+            'profil_bio' => 'http://vinssalon.blogspot.com/',
+            'foto_profil' => '',
+            'tempat_kerja' => 'Terisi',
+            'status' => 'online',
+            'role' => 'mua',
         ]);
     }
 }
